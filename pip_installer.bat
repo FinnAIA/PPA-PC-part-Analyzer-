136fd7755042979e68ea63bd4cc5f138a969ae57
FinnAIA <144200185+FinnAIA@users.noreply.github.com>
2024-02-11 13:48:55 +0800

    Update pip_installer.bat

--- pip_installer.bat
+++ pip_installer.bat
@@ -2,8 +2,7 @@
 title pip installer.bat
 py -m pip install --upgrade pip
 py -m pip install psutil
-py -m pip install tkinter
 py -m pip install socket
-py -m pip install subprocess
+py -m pip install getpass
 timeout -t 5 <nul
 exit
